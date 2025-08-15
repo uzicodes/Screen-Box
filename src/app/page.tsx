f8037395fcf97d00dc89071a0f60f6b05cd463a7
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-08-15 23:42:45 +0600

    added font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center w-full">
         <img src="/logo.png" alt="Logo" className="mx-auto mb-6 w-32 h-32 object-contain" />
-        <h1 className="text-4xl font-bold">Welcome to Screen Box</h1>
+        <h1 className="text-7xl font-bold">SCREEN  BOX</h1>
       </div>
       <p className="mt-4">Your movie/webseries streaming web app is ready!</p>
     </main>
