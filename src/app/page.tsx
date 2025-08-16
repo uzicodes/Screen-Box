eed18186d7096faac6746da4db1a664388203536
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-08-16 21:07:02 +0600

    increase main heading size and add subheading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,9 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#2dd4bf] to-[#1f2937]">
         <div className="flex flex-col items-center w-full mt-20">
           <img src="/logo.png" alt="Logo" className="mx-auto mb-6 w-32 h-32 object-contain" />
-          <h1 className="text-7xl font-bold">SCREEN  BOX</h1>
+          <h1 className="text-8xl font-bold">SCREEN  BOX</h1>
+          <br></br>
+          <h2 className="text-2xl">nothing to pay, Enjoy !</h2>
         </div>
       </main>
     </>
