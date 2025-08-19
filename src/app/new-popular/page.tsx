c61e49e5fd4dfabe36349daa9c7da002caeb6415
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-08-19 20:57:11 +0600

    fixed some vercel ESlint errors

---
+++ src/app/new-popular/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NewPopularPage() {
+	return (
+		<div className="p-8 text-white">
+			<h1 className="text-3xl font-bold mb-4">New & Popular</h1>
+			<p>Welcome to the New & Popular page! Add your content here.</p>
+		</div>
+	);
+}
