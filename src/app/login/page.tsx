3a0a3013d35a4ff0def9010a699f442dc49ccf23
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-08-19 22:33:34 +0600

    some tweaking at the login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,8 +18,8 @@ export default function LoginPage() {
     <div style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center", background: "#18181b" }}>
       <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.title}>
-          Login<br />
-          <span>Welcome back! Please login to your account.</span>
+          LOGIN<br />
+          <span>Welcome back!</span>
         </div>
         <input
           type="email"
@@ -37,14 +37,10 @@ export default function LoginPage() {
           className={styles.input}
           placeholder="Password"
         />
-        <div className={styles["login-with"]}>
-          <button type="button" className={styles["button-log"]}>
-            <svg className={styles.icon} viewBox="0 0 24 24"><path d="M21.35 11.1H12v2.8h5.35c-.23 1.17-1.4 3.43-5.35 3.43-3.23 0-5.88-2.68-5.88-5.98s2.65-5.98 5.88-5.98c1.84 0 3.07.79 3.77 1.47l2.58-2.51C17.07 3.99 14.87 3 12 3 6.48 3 2 7.48 2 13s4.48 10 10 10c5.52 0 10-4.48 10-10 0-.68-.07-1.34-.2-1.9z"/></svg>
-          </button>
-          <button type="button" className={styles["button-log"]}>
-            <svg className={styles.icon} viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12c0 4.84 3.87 8.82 8.65 9.75v-6.9H8.9v-2.85h1.75V9.7c0-1.73 1.04-2.68 2.62-2.68.76 0 1.56.13 1.56.13v1.72h-.88c-.87 0-1.14.54-1.14 1.09v1.31h2.1l-.34 2.85h-1.76v6.9C18.13 20.82 22 16.84 22 12c0-5.52-4.48-10-10-10z"/></svg>
-          </button>
+        <div style={{ width: "100%", textAlign: "center", marginTop: "10px", color: "#ccc", fontFamily: "Lora, serif" }}>
+            Don't have an account? <a href="/register" style={{ color: "#F5B727", textDecoration: "underline", fontWeight: 600 }}>REGISTER !</a>
         </div>
+  {/* ...logos removed... */}
         {error && <p style={{ color: "#e74c3c", textAlign: "center" }}>{error}</p>}
         <button type="submit" className={styles["button-confirm"]}>Login</button>
       </form>
