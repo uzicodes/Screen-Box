95c95a0e1ccc9fc339b45cc98ba369c322530844
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-09-17 19:54:18 +0600

    renamed a section of homepage aas 'anime recommendations'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -231,7 +231,7 @@ export default function Home() {
 
         {/* Popular this Month */}
   <section className="relative w-full mt-12 z-10">
-          <h3 className="text-3xl font-bold text-white mb-6 ml-4">Popular this Month</h3>
+          <h3 className="text-3xl font-bold text-white mb-6 ml-4">Anime Recommendations</h3>
           <div className="flex overflow-x-auto gap-6 px-4 pb-4 hide-scrollbar">
             {[{
               title: "Dummy Rec 1",
